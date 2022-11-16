5b20c20bd4c6441433bccd8baaba7bb40f387327
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-17 00:03:24 +0500

    footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light text-center p-5'>Footer</div>
+    <div className='bg-dark text-light text-center p-5 h1'>Footer</div>
   )
 }
 
