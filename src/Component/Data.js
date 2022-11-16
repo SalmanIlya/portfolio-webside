2fffc1d38c40312452a84838e2e8b374cbad2a45
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-16 16:55:48 +0500

    create-small-api

--- src/Component/Data.js
+++ src/Component/Data.js
@@ -1,26 +1,27 @@
 const web=[
     {
         id:1,
-        image:"",
+        image:"image/1.jpg",
         githubLink:"",
         webLink:""
     },
     {
-        id:1,
-        image:"",
+        id:2,
+        image:"image/1.jpg",
         githubLink:"",
         webLink:""
     },
     {
-        id:1,
-        image:"",
+        id:3,
+        image:"image/1.jpg",
         githubLink:"",
         webLink:""
     },
     {
-        id:1,
-        image:"",
+        id:4,
+        image:"image/1.jpg",
         githubLink:"",
         webLink:""
     }
 ]
+export default web
