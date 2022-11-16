918abb34501d2e30ffe2351d109b6688ed132330
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-16 16:55:08 +0500

    create-about-page

---
+++ src/Component/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Style.css"
+const About = () => {
+  return (
+    <div>
+        <h2 className='text-center'><strong>About</strong>
+<div className='h3-div'></div></h2>
+    </div>
+  )
+}
+
+export default About
