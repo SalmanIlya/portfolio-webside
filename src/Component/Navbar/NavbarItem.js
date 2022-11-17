eb97120ce0512491d7d94ae6115b84a22422da18
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-17 12:02:01 +0500

    update in navbar

--- src/Component/Navbar/NavbarItem.js
+++ src/Component/Navbar/NavbarItem.js
@@ -1,34 +1,25 @@
 import React from 'react'
-
 const Navbaritem = ({contact,about}) => {
   return (
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className='container'>
 
-  <div class="container d-flex flex-row justify-between ">
-    <div>
-    <a class="navbar-brand" href="#">Navbar</a>
-    </div>
-<div>
-
- 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0  ">
-        <li class="nav-item m-1 nav-link" onClick={()=>{contact()}}>
-       Contact
-      
+    <a className="navbar-brand text-light ms-5" href="#">Navbar</a>
+    <button className="navbar-toggler  bg-light me-5" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul className="navbar-nav    ms-auto   ">
+        <li className="nav-item active nav-link btn    m-3 text-light" onClick={()=>{about()}}>
+        About
         </li>
-        <li class="nav-item m-1 nav-link " onClick={()=>{about()}}>
-          About
+        <li className="nav-item active nav-link btn   m-3 text-light" onClick={()=>{contact()}}>
+         Contact
         </li>
       </ul>
     </div>
-</div>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </div>
-  
-</nav>
+    </div>
+  </nav>
   )
 }
 
