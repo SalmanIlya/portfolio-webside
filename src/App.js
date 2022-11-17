242d6e55d31c0a2a643176e6a8b351bb7de547d9
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-17 12:03:26 +0500

    small-update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Contact from './Component/Contact/Contact'
 import Footer from './Component/Footer/Footer'
 import HeroSection from './Component/Navbar/HeroSection'
 import Websides from './Component/Webside/Websides'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./App.css"
 const App = () => {
   return (
